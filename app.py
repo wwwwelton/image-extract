8036b310604a3a7e6a2844fb0cdcedc793cad855
wleite <wleite@student.42sp.org.br>
2023-05-26 15:56:06 -0300

    test: adicionado tests com invoke

--- app.py
+++ app.py
@@ -5,20 +5,18 @@ from deep_translator import GoogleTranslator
 import boto3
 from io import BytesIO
 from faker import Faker
-
+import os
 
 app = Chalice(app_name='image-extract')
 
 @app.lambda_function()
 def first_function(event, context):
+
     downloadImage = requests.get(event['URL'])
 
-    # filename = "test.jpg"
-    # data = {'file': open(filename, 'rb')}
-
-    # dicionario json com o conteúdo do arquivo (USAMOS UMA VARIÁVEL DUPLA PARA NOMEAR O ARQUIVO)
+    # dicionario json com o conteúdo do arquivo (USAMOS UMA VARIÁVEL TUPLA PARA NOMEAR O ARQUIVO)
     data = {'file': ("image.jpg", downloadImage.content)}
-    
+
     # extraíndo o texto da imagem com a freeOCRapi
     sendImage = requests.post("https://freeocrapi.com/api", files=data)
     jsonTemp = json.loads(sendImage.content)
@@ -27,8 +25,12 @@ def first_function(event, context):
     # traduzindo o texto para o inglês
     texto_em_ingles = GoogleTranslator(source='auto', target='en').translate(texto_original)
 
-    #criar o client() para fazer a conexão s3
-    s3_connection = boto3.client('s3', endpoint_url=("http://host.docker.internal:4566"))
+    # criar o client() para fazer a conexão s3 com LocalStack
+    aws_environment = os.environ.get("AWS_LAMBDA_FUNCTION_VERSION", "PY_TEST")
+    if aws_environment == "PY_TEST":
+        s3_connection = boto3.client('s3', endpoint_url=("http://localhost:4566"))
+    else:
+        s3_connection = boto3.client('s3', endpoint_url=("http://host.docker.internal:4566"))
 
     texto_em_ingles = texto_em_ingles.encode('utf-8')
     upload_bucket = BytesIO(texto_em_ingles)
@@ -38,10 +40,13 @@ def first_function(event, context):
     # s3_connection.Object('mybucket', upload_bucket).put(Body=upload_bucket)
     return {'Texto da imagem traduzido para o ingles': texto_em_ingles}
 
-# Listar arquivos do bucket 
+# Listar arquivos do bucket
 # awslocal s3 ls s3://mybucket
 
 # awslocal s3 rm s3://mybucket/his.flac
+
+# limpar o bucket
+# awslocal s3 rm s3://mybucket --recursive
 
 # Ler conteúdo do arquivo
 # awslocal s3 cp s3://mybucket/tradução - | cat
@@ -81,3 +86,34 @@ def first_function(event, context):
 
 # Updating policy for IAM role: image-extract-dev
 # Updating lambda function: image-extract-dev-first_function
+
+# echo "{\"URL\":\"https://cdn.pensador.com/img/imagens/pe/ns/pensador_mensagens_de_bom_dia_21.jpg\"}" | chalice-local invoke -n first_function
+
+# echo '{"URL":"https://cdn.pensador.com/img/imagens/pe/ns/pensador_mensagens_de_bom_dia_21.jpg"}' | chalice-local invoke -n first_function
+
+
+
+
+# Apresentação
+
+# Link de uma imagem
+# https://www.delcampe.net/static/img_large/auction/001/627/557/137_002.jpg?v=1
+
+# Chamar a função Lambda com invoke no terminal
+# echo '{"URL":"https://www.delcampe.net/static/img_large/auction/001/627/557/137_002.jpg?v=1"}' | chalice-local invoke -n first_function
+
+# Listar os buckets
+# awslocal s3 ls
+
+# Lista os arquivos do bucket
+# awslocal s3 ls s3://mybucket/
+
+# Listar o conteúdo do arquivo do bucket,
+# awslocal s3 cp s3://mybucket/NOME_DO_ARQUIVO_DO_BUCKET - | cat
+
+# Copiar o arquivo do bucket para a pasta local
+# awslocal s3 cp s3://mybucket/NOME_DO_ARQUIVO_DO_BUCKET test.txt
+# cat test.txt
+
+# Rodar os testes
+# py.test tests/test_app.py
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,37 @@
 from chalice.test import Client
 from app import app
 
+# https://cdn.pensador.com/img/imagens/pe/ns/pensador_mensagens_de_bom_dia_21.jpg
 
-def test_index():
+
+def test_mensagens_de_bom_dia():
     with Client(app) as client:
-        response = client.lambda_.invoke('first_function', {})
-        assert response.payload == {'hello': 'world'}
+        result = client.lambda_.invoke(
+            'first_function', {'URL': 'https://cdn.pensador.com/img/imagens/pe/ns/pensador_mensagens_de_bom_dia_21.jpg'})
+        expected_str = str.encode(
+            'Good morning!\nMay your day be light\nand full of smiles.', "utf-8")
+        assert result.payload == {'Texto da imagem traduzido para o ingles': expected_str}
+
+
+def test_sigmund_freud():
+    with Client(app) as client:
+        result = client.lambda_.invoke(
+            'first_function', {'URL': 'https://cdn.pensador.com/img/frase/si/gm/sigmund_freud_minha_cultura_e_minhas_conquistas_sao_ale_trf_lqvg98e.jpg'})
+        expected_str = str.encode('My culture and my\nAchievements are German.\nIntellectually\nI considered myself German until\nrealize that the\nantisemitic prejudices\nwere increasing in\nGermany and Austria. A\ngo away, leave me\nconsider german. I prefer\ndefine myself as a Jew.\n66 Baa tne Sigmund Freud', "utf-8")
+        assert result.payload == {'Texto da imagem traduzido para o ingles': expected_str}
+
+
+def test_italian_phrase():
+    with Client(app) as client:
+        result = client.lambda_.invoke(
+            'first_function', {'URL': 'https://i.pinimg.com/236x/cf/7e/da/cf7eda50793451d65a4b098ac0771992.jpg'})
+        expected_str = str.encode('To see the real face\nof a person you just have to\nwait for the time\nconsume the mask that\nwears.\nAmey', "utf-8")
+        assert result.payload == {'Texto da imagem traduzido para o ingles': expected_str}
+
+def test_italian():
+    with Client(app) as client:
+        result = client.lambda_.invoke(
+            'first_function', {'URL': 'https://www.delcampe.net/static/img_large/auction/001/627/557/137_002.jpg?v=1'})
+        expected_str = str.encode('YOU COME DOWN FROM THE STARS\nYou come down from the stars,\no King of heaven,\nand come to a cave\nin the cold and frost.\n\u00a9 My divine child,\n10 I see you here trembling.\nO blessed God!\nAh, how much it cost you\nhaving loved me.\nTo you who are of the world\nthe creator\nclothes and fire are missing,\nOh my Lord.\nDear elected little boy\nhow much this poverty\nthe more I fall in love\njackets made you love\nstill poor.\nGuido Reni, Adoration of the Shepherds\nwww.delcampe.net\norchard', "utf-8")
+        assert result.payload == {
+            'Texto da imagem traduzido para o ingles': expected_str}
